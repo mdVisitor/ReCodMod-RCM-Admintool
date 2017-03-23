759f975ef521378ed1a6dd32101044e630d4b040
Erick <yaeriks@yandex.ru>
2017-03-23 22:09:16 +0200

    Update ban.php

--- RCM/ReCodMod/plugins/adm_plugins/ban.php
+++ RCM/ReCodMod/plugins/adm_plugins/ban.php
@@ -64,8 +64,10 @@ if ($x_stop_lp == 0) {
 								
 								if(empty($a_guidcc))
 									$a_guidcc = $x_nickx;
+				                                      else
+				                             $a_guidcx = $nickr.' - '.$a_guidcc;
 								
-                                $db2->exec("INSERT INTO bans (playername,ip,guid,reason,time,whooo,patch) VALUES ('$x_bann[1]','$x_bann[2]','$i_guid','$x_reason','$datetime','$a_guidcc','$game_patch')");
+                                $db2->exec("INSERT INTO bans (playername,ip,guid,reason,time,whooo,patch) VALUES ('$x_bann[1]','$x_bann[2]','$i_guid','$x_reason','$datetime','$a_guidcx','$game_patch')");
                                 if ($x_stop_lp == 0) {
                                     usleep($sleep_rcon);
                                     rcon('say  ^6 ' . $chistx . ' ' . $ban_ip_all . ' ^7' . $infooreas . ': ^1' . $x_reason . '', '');
