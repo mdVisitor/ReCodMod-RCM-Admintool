33f01bd1d3828aef486c4cc5aed7c7899ac02570
Erick <yaeriks@yandex.ru>
2019-02-04 09:38:27 +0200

    Update _connection.php

--- RCM/cfg/_connection.php
+++ RCM/cfg/_connection.php
@@ -10,9 +10,27 @@ $server_rconpass = '123';
 $mplogfile = "C:\Games\Call of Duty 4 - Modern Warfare\mods\promodzzz\games_mp.log";  //game log file
 
 											
-//=======================================================================
-// Create new webhook in your Discord channel settings and copy&paste URL
-$webhookurl = "https://discordapp.com/api/webhooks/.............";
+//=======================  Discord REPORT AND SUPORT  =======================
+//===========================================================================
+/* Create new webhook in your Discord channel settings and copy&paste URL                */
+
+$webhookurl = "https://discordapp.com/api/webhooks/.............";   // если пусто, типо так $webhookurl = ""; значит отключили
+
+
+//======================= VKONTAKTE REPORT AND SUPORT =======================
+//===========================================================================
+/*
+ Create new token in your vkontakte.com
+ https://vkhost.github.io/ => VK API => 
+Инструкция
+    1.Выберите приложение
+    2.Нажмите на него
+    3.Затем нажмите "разрешить"
+    4.Скопируйте часть адресной строки от access_token= до &expires_in                         */
+
+$vk_token = "a82abcb1f135ac36c581aef71830049cd4afb00ce7897e13f1a613339a7dfa1e74e818eb7f0b2f2c0c27e"; //наш токен
+// если пусто, типо так $vk_token = ""; значит отключили
+$vk_owners = array("353126441");  // Можно посылать другим:  array("35312467846441","3465657647","567856869","346356735735","3737373575373");
 
 
 ?>
